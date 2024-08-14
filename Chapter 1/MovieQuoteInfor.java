5b4f1f9e73d35dcd9cc33b68241edf7afb81cb42
Jazelle23 <jazellet5@gmail.com>
2024-08-15 00:50:48 +1000

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,16 @@
+
+public class 6 {
+    public static void main(String[] args) {
+        String quote =  "There's no way we could meet. But one thing is certain. If we see each other, we'll know. That you were the one who was inside me. That I was the one who was inside you.";
+        String movie = "Your name";
+        String character = "Taki Tachibana and Mitsuha Miyamizu";
+        String year = "2016";
+
+        System.out.println("Movie Quote: " +quote);
+        System.out.println("Movie Title: " +movie);
+        System.out.println("Character: " +character);
+        System.out.println("Year: " +year);
+
+    }
+}
+
