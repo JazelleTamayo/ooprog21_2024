5fb6445cbabaceb3dd5075ec57bfea8c9c28eb19
Jazelle23 <jazellet5@gmail.com>
2024-08-16 07:57:28 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -1,7 +1,7 @@
 
 public class TableAndChairs{
     public static void main(String[] args){
-          System.out.println("*                      *");
+          System.out.println("X                      X");
           System.out.println("*                      *");
           System.out.println("*      **********      *");
           System.out.println("*****  *        *  *****");
