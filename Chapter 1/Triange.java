342504767c86ca1934829649c32f729bdbd6f195
Jazelle23 <jazellet5@gmail.com>
2024-08-16 08:40:56 +0800

    Update Triange.java

--- Chapter 1/Triange.java
+++ Chapter 1/Triange.java
@@ -1,16 +1,11 @@
 public class Triange {
     public static void main(String[] args) {
-        int rows = 7; 
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows + i; j++) {
-                if (j >= rows - 1 - i && j <= rows - 1 + i) {
-                    System.out.print("T");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+        System.out.println("        T");
+        System.out.println("       TTT");
+        System.out.println("      TTTTT");
+        System.out.println("     TTTTTTT");
+        System.out.println("    TTTTTTTTT");
+        System.out.println("   TTTTTTTTTTT");
+        System.out.println("  TTTTTTTTTTTTT");
     }
 }
