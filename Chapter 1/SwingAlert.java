293f5b8833c6f6f6c28a8d1895905a33b46afa2a
JazelleTamayo <jazellet5@gmail.com>
2024-08-21 19:16:04 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+
+      public static void main(String[] args) {
+
+         String message = "Hello World";
+    
+         JOptionPane.showMessageDialog(null, message);
+   
+   }
+   
+}
+   
+   
