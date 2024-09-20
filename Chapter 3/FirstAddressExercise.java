c96b080620d63c530f56541e72a5f3d435535b2b
JazelleTamayo <jazellet5@gmail.com>
2024-09-20 21:23:52 +0800

    FirstAddressExercise.java

--- Chapter 3/FirstAddressExercise.java
+++ Chapter 3/FirstAddressExercise.java
@@ -1,6 +1,6 @@
 public class FirstAddressExercise {
     public static void main(String[] args) {
-        System.out.println("First Java application");
+        System.out.println("First Java application\n");
         displayAddress();
     }
 
