cb16d40b5868cf79847f817b117dc761fba2340e
JazelleTamayo <jazellet5@gmail.com>
2024-09-20 20:14:15 +0700

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,13 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("BBK Electronics");
+        System.out.println("No. 283 BBK Road");
+        System.out.println("Wusha, Chang'an, Dongguan, Guangdong 523860");  
+        System.out.println("Company : Vivo Communication Technology Co., Ltd.");     
+    }
+}
